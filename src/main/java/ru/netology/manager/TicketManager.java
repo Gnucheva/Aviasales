32caf832481102bb26e191ecd918857e8641dbcb
Anastasia <n41111215@mail.ru>
2020-12-02 16:49:11 +0300

    add TicketManager.java with comparator

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -5,6 +5,7 @@ import ru.netology.repository.TicketRepository;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class TicketManager {
     private TicketRepository repository;
@@ -20,7 +21,6 @@ public class TicketManager {
     public Ticket[] findAllByPrice(String from, String to) {
         Ticket[] result = new Ticket[0];
         for (Ticket ticket : repository.getAll()) {
-            int length = result.length;
             if (ticket.getDeparture().equalsIgnoreCase(from) && ticket.getArrival().equalsIgnoreCase(to)) {
                 Ticket[] tmp = new Ticket[result.length + 1];
                 System.arraycopy(result, 0, tmp, 0, result.length);
@@ -33,10 +33,19 @@ public class TicketManager {
         return result;
     }
 
-    public Ticket[] showOffers() {
-        Ticket[] result = repository.getAll();
-        Arrays.sort(result);
+    public Ticket[] findAllByTime(String from, String to, Comparator<Ticket> comparator) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket : repository.getAll()) {
+            if (ticket.getDeparture().equalsIgnoreCase(from) && ticket.getArrival().equalsIgnoreCase(to)) {
+                Ticket[] tmp = new Ticket[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                int lastIndex = tmp.length - 1;
+                tmp[lastIndex] = ticket;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result, comparator);
         return result;
-
     }
 }
+
