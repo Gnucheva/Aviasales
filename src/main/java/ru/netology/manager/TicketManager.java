1ab34c16a6130cb940b6506e61700ce0da8204e4
Anastasia <n41111215@mail.ru>
2020-12-02 16:33:59 +0300

    edit TicketManager.java

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -13,22 +13,17 @@ public class TicketManager {
         this.repository = repository;
     }
 
-    public boolean matches(Ticket ticket, String from, String to) {
-        return ticket.getDeparture().equalsIgnoreCase(from)
-                && ticket.getArrival().equalsIgnoreCase(to);
-    }
-
     public void add(Ticket ticket) {
         repository.save(ticket);
     }
 
-    public Ticket[] findAll(String from, String to) {
+    public Ticket[] findAllByPrice(String from, String to) {
         Ticket[] result = new Ticket[0];
         for (Ticket ticket : repository.getAll()) {
             int length = result.length;
-            if (ticket.getDeparture().equals(from) && ticket.getArrival().equals(to)) {
-                Ticket[] tmp = new Ticket[length + 1];
-                System.arraycopy(result, 0, tmp, 0, length);
+            if (ticket.getDeparture().equalsIgnoreCase(from) && ticket.getArrival().equalsIgnoreCase(to)) {
+                Ticket[] tmp = new Ticket[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
                 int lastIndex = tmp.length - 1;
                 tmp[lastIndex] = ticket;
                 result = tmp;
